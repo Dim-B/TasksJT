cac22db7b55bb4b2e6d9fe5b576ecfe6c929137e
Dim-B <dimon0013@mail.ru>
2017-08-22 12:17:43 +0300

    rename class z5

--- src/main/java/Z5_Простые_числа/SimpleNumbers.java
+++ src/main/java/Z5_Простые_числа/SimpleNumbers.java
@@ -6,7 +6,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class PrimeNumbers {
+public class SimpleNumbers {
     public static void main(String[] args) throws IOException {
         int n;
         int m;
